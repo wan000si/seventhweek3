b1249174eeacff98a42435fff9750fdf4017c2c2
wan_wansi <576529493@qq.com>
2018-06-07 14:08:46 +0800

    add Company.java

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,54 @@
 package com.example.employee.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "company")
 public class Company {
+    @Id
+//    @GeneratedValue
+    private int id;
+    @Column(nullable = false)
+    private String companyName;
+    @Column(nullable = false)
+    private int employeesNumber;
+
+
+    public Company() {
+
+    }
+
+    public Company(int id, String companyName, int employeesNumber) {
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 
 }
