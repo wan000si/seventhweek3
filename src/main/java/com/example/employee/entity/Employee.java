e6a4ba590acf07a37ee83e0fad9fffc197167fc2
wan_wansi <576529493@qq.com>
2018-06-07 14:10:53 +0800

    add Employee.java

--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,94 @@
 package com.example.employee.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int age;
+    private String gender;
+    @Column(nullable = false)
+    private int salary;
+    @Column(nullable = false)
+    private int companyId;
+
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, int age, String gender, int salary, int companyId,int id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", salary=" + salary +
+                ", companyId=" + companyId +
+                '}';
+    }
 
 }
